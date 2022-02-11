38424e1663789986bce4a9ef7647c86e49c62c42
jvdionizio <dionizio145@gmail.com>
2022-02-11 16:08:13 -0300

    objeto results tratado

--- script.js
+++ script.js
@@ -5,24 +5,23 @@ let marker;
 let geocoder;
 let responseDiv;
 let response;
-// let markers = [];
 
-function getLocation() {
-  if (!navigator.geolocation) {
-    return null;
-  }
+// function getLocation() {
+//   if (!navigator.geolocation) {
+//     return null;
+//   }
 
-  navigator.geolocation.getCurrentPosition((pos) => {
-    lat = document.getElementById("lat").innerText = pos.coords.latitude;
-    lon = document.getElementById("lon").innerText = pos.coords.longitude;
-    initMap();
-  });
-}
+//   navigator.geolocation.getCurrentPosition((pos) => {
+//     lat = document.getElementById("lat").innerText = pos.coords.latitude;
+//     lon = document.getElementById("lon").innerText = pos.coords.longitude;
+//     initMap();
+//   });
+// }
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: { lat: -34.397, lng: 150.644 },
+    zoom: 2,
+    center: { lat: 24.409102, lng: 19.856894 },
     mapTypeId: "terrain",
     mapTypeControl: false,
   });
@@ -81,33 +80,17 @@ function initMap() {
   });
 
   clear();
-
-  // const geocoder = new google.maps.Geocoder();
-
   const infowindow = new google.maps.InfoWindow();
 
   document.getElementById("submit").addEventListener("click", () => {
     geocodeLatLng(geocoder, map, infowindow);
   });
-
-  // google.maps.event.addListener(map, 'click', function (event) {
-  //   addMarker(event.latLng);
-  // });
-
-  // map.addListener('click', (event) => {
-  //   addMarker(event.latLng);
-  // })
-
-  // document.getElementById('show-markers').addEventListener('click', showMarkers);
-
-  // document.getElementById('hide-markers').addEventListener('click', hideMarkers);
-
-  // document.getElementById('delete-markers').addEventListener('click', deleteMarkers);
 }
+
+initMap();
 
 function clear() {
   marker.setMap(null);
-  responseDiv.style.display = "none";
 }
 
 
@@ -119,95 +102,42 @@ function geocode(request) {
     map.setCenter(results[0].geometry.location);
     marker.setPosition(results[0].geometry.location);
     marker.setMap(map);
-    responseDiv.style.display = "block";
-    response.innerText = JSON.stringify(result, null, 2);
-    const obj = JSON.stringify(result, null, 2);
-    findWeather(obj)
-    // console.log('responseDiv: ' + responseDiv.innerText);
-    // console.log('response: ' + response.innerText);
-
+    const objStg = JSON.stringify(results, null, 2);
+    const objParse = JSON.parse(objStg);
+    findPlace(objParse)
     return results;
   }).catch((e) => {
     alert("Geocode was not successful for the following reason: " + e);
   });
 }
 
-getLocation();
-
-function findWeather(obj) {
-  let myObj = obj;
-  console.log(JSON.parse(myObj));
-  //   myObj.find((JSON.parse(element)) => {
-  //     return(element === responseDiv.innerText.results)
-  // })
+function stringContainsNumber(_string) {
+  let matchPattern =_string.match(/\d+/g);
+  if (matchPattern != null) {
+    return true
+   } else{
+    return false
+  }
 }
 
-// function addMarker(location) {
-//   const marker = new google.maps.Marker({
-//     position: location,
-//     map: map,
-//   });
-
-//   markers.push(marker);
-
-//   google.maps.event.addListener(marker, "click", function (event) {
-//     let latitude = event.latLng.lat();
-//     let longitude = event.latLng.lng();
-//     console.log(latitude + ', ' + longitude);
-
-//     // radius = new google.maps.Circle({
-//     //   map: map,
-//     //   radius: 100,
-//     //   center: event.latLng,
-//     //   fillColor: '#777',
-//     //   fillOpacity: 0.1,
-//     //   strokeColor: '#AA0000',
-//     //   strokeOpacity: 0.8,
-//     //   strokeWeight: 2,
-//     //   draggable: true,    // Dragable
-//     //   editable: true      // Resizable
-//     // });
-
-//     // Center of map
-//     map.panTo(new google.maps.LatLng(latitude, longitude));
-
-//   });
-// }
-
-// function geocodeLatLng(geocoder, map, infowindow) {
-//   const input = document.getElementById("latlng").value;
-//   const latlngStr = input.split(",", 2);
-//   const latlng = {
-//     lat: parseFloat(latlngStr[0]),
-//     lng: parseFloat(latlngStr[1]),
-//   };
-
-//   geocoder
-//     .geocode({ location: latlng })
-//     .then((response) => {
-//       if (response.results[0]) {
-//         map.setZoom(11);
-
-//         const marker = new google.maps.Marker({
-//           position: latlng,
-//           map: map,
-//         });
-
-//         infowindow.setContent(response.results[0].formatted_address);
-//         infowindow.open(map, marker);
-//       } else {
-//         window.alert("No results found");
-//       }
-//     })
-//     .catch((e) => window.alert("Geocoder failed due to: " + e));
-// }
-
-// Sets the map on all markers in the array.
-// function setMapOnAll(map) {
-//   for (let i = 0; i < markers.length; i++) {
-//     markers[i].setMap(map);
-//   }
-// }
+function findPlace(nearLocations) {
+  console.log(nearLocations);
+  const nearstLocation = nearLocations[0];
+  const nextLocation = nearLocations[1];
+  if (!stringContainsNumber(nearstLocation.formatted_address[0])) {
+    const stateObj = nearstLocation.address_components.find((element) => element.types.includes('administrative_area_level_1'));
+    const getStateName = () => {
+      if (stateObj) {
+        return stateObj.long_name
+      }
+      return '';
+    };
+    const state = getStateName();
+    const countryObj = nearstLocation.address_components.find((element) => element.types.includes('country'));
+    const country = countryObj.long_name;
+    console.log(state + ', ' + country);
+  };
+}
 
 // Shows any markers currently in the array.
 function showMarkers() {
