c9df0cd52e7a5a26386f47baa07b43abbb8c7080
jvdionizio <dionizio145@gmail.com>
2022-02-13 03:05:52 -0300

    Adicionando informações do geodatasource

--- script.js
+++ script.js
@@ -6,20 +6,30 @@ let geocoder;
 let responseDiv;
 let response;
 const container = document.querySelector('.container');
+const geoContainer = document.querySelector('.geo-container');
+const geoBtn = document.querySelector('#geodata-btn');
 
-function getLocalStorage() {
-  const previousList = getSavedInfo();
-  // const list = JSON.parse(previousList)
-  // console.log(list);
-  container.innerHTML = previousList;
-  autoSaveItems();
-}
+geoBtn.addEventListener('click', () => {
+  geoContainer.style.display = 'flex';
+})
+// function getLocalStorage() {
+//   const previousList = getSavedInfo();
+//   // const list = JSON.parse(previousList)
+//   // console.log(list);
+//   container.innerHTML = previousList;
+//   autoSaveItems();
+// }
 
-getLocalStorage();
+// getLocalStorage();
+
+
 
 function clearweatherInfo() {
   const weatherInfo = document.querySelector('.weather-info');
+  const geoInfo = document.querySelector('.geo-info');
   weatherInfo.remove();
+  geoInfo.remove();
+  geoContainer.style.display = 'none';
 }
 
 
@@ -85,11 +95,18 @@ function clear() {
   marker.setMap(null);
 }
 
-function autoSaveItems() {
-  const items = container.innerHTML
-  // localList = JSON.stringify(items);
-  saveInfoItems(items);
-  console.log(getSavedInfo());
+// function autoSaveItems() {
+//   const items = container.innerHTML
+//   // localList = JSON.stringify(items);
+//   saveInfoItems(items);
+//   console.log(getSavedInfo());
+// }
+
+function getIcon(infoType) {
+  const element = document.createElement('img');
+  element.classList.add(`${infoType}-icon`);
+  element.src = `./images/${infoType}.png`;
+  return element;
 }
 
 function createTemp(objElement) {
@@ -105,6 +122,7 @@ function createUm(objElement) {
   const element = document.createElement('div');
   element.classList.add('um');
   element.innerText = um;
+  element.appendChild(getIcon('um'));
   return element;
 }
 
@@ -113,6 +131,7 @@ function createPol(objElement) {
   const element = document.createElement('div');
   element.classList.add(classe);
   element.innerText = pol;
+  element.appendChild(getIcon('pol'));
   return element;
 }
 
@@ -160,18 +179,16 @@ function colorDiv(pollution) {
 function addWeatherElements(objWeather) {
   if (objWeather.status === 'fail') {
     const response = {
-      type: 'div',
       classe: 'weather_fail',
       text: 'Infelizmente não conseguimos uma estação meteorológica próxima a esta região',
       img: 'lupa'
     }
     container.appendChild(infoItem(response));
-    autoSaveItems();
+    // autoSaveItems();
   } else {
     const weather = objWeather.data.current.weather;
     const pollution = objWeather.data.current.pollution;
     const response = {
-      type: 'div',
       classe: colorDiv(pollution.aqius),
       temp: `${weather.tp}°C`,
       um: `${weather.hu}%`,
@@ -179,45 +196,96 @@ function addWeatherElements(objWeather) {
       img: weather.ic
     }
     container.appendChild(infoItem(response));
-    autoSaveItems();
+    const geoBtn = document.querySelector('#geodata-btn');
+    geoBtn.style.display = 'block';
+    // autoSaveItems();
   }
+}
+
+function createLocation(objElement) {
+  const { cnt, rg, ct } = objElement;
+  const element = document.createElement('div');
+  element.classList.add('location');
+  element.innerText = `${ct}, ${rg}, ${cnt}`;
+  element.appendChild(getIcon('marker'));
+  return element;
+}
+
+function createCurrency(objElement) {
+  const { cc, cs } = objElement;
+  const element = document.createElement('div');
+  element.classList.add('currency');
+  element.innerText = `${cc} || ${cs}`;
+  element.appendChild(getIcon('coin'));
+  return element;
+}
+
+function createSun(objElement) {
+  const { sr, ss } = objElement;
+  const element = document.createElement('div');
+  element.classList.add('sun');
+  element.innerText = `${sr} ➙ ${ss}`;
+  element.appendChild(getIcon('sun'));
+  return element;
+}
+
+function createTz(objElement) {
+  const { tz } = objElement;
+  const element = document.createElement('div');
+  element.classList.add('tz');
+  element.innerText = tz;
+  element.appendChild(getIcon('tz'));
+  return element;
+}
+
+
+
+function dataItem(objInfos) {
+  const geoInfo = document.createElement('div');
+  geoInfo.classList.add('geo-info');
+  geoInfo.appendChild(createLocation(objInfos));
+  geoInfo.appendChild(createCurrency(objInfos));
+  geoInfo.appendChild(createSun(objInfos));
+  geoInfo.appendChild(createTz(objInfos));
+  return geoInfo;
 }
 
 function addGeoDataElements(objGeoData) {
-  if (!objGeoData) {
+  const { country, region, city, currency_code, currency_symbol, sunrise, sunset, time_zone, error } = objGeoData
+  if (error) {
 
   } else {
-    const arrayNode = arrayInfo.map(() => document.createElement('li'));
-    arrayNode.forEach((element) => element.className = 'item-info');
-    arrayNode[0].innerText = `País: ${arrayInfo[0]}`;
-    arrayNode[1].innerText = `Região: ${arrayInfo[1]}`;
-    arrayNode[2].innerText = `Cidade: ${arrayInfo[2]}`;;
-    arrayNode[3].innerText = `Código da Moeda: ${arrayInfo[3]}`;
-    arrayNode[4].innerText = `Símbolo da Moeda: ${arrayInfo[4]}`;
-    arrayNode[5].innerText = `Nascer do Sol: ${arrayInfo[5]}`;
-    arrayNode[6].innerText = `Pôr do Sol: ${arrayInfo[6]}`;
-    arrayNode[7].innerText = `Fuso Horário: ${arrayInfo[7]}`;
-    return arrayNode;
+    const response = {
+      cnt: country,
+      rg: region,
+      ct: city,
+      cc: currency_code,
+      cs: currency_symbol,
+      sr: sunrise,
+      ss: sunset,
+      tz: time_zone,
+    }
+    geoContainer.appendChild(dataItem(response));
   }
 }
 
-function makeRegionInfos(objInfos) {
-  const { country, region, city, currency_code, currency_symbol, sunrise, sunset, time_zone } = objInfos;
-  const arrayInfo = [];
-  const unorList = document.createElement('ul');
-  unorList.id = 'region-info';
-  arrayInfo.push(country);
-  arrayInfo.push(region);
-  arrayInfo.push(city);
-  arrayInfo.push(currency_code);
-  arrayInfo.push(currency_symbol);
-  arrayInfo.push(sunrise);
-  arrayInfo.push(sunset);
-  arrayInfo.push(time_zone);
-  const arrayNode = makeNodes(arrayInfo);
-  arrayNode.forEach((element) => unorList.appendChild(element));
-  return unorList;
-}
+// function makeRegionInfos(objInfos) {
+//   const { country, region, city, currency_code, currency_symbol, sunrise, sunset, time_zone } = objInfos;
+//   const arrayInfo = [];
+//   const unorList = document.createElement('ul');
+//   unorList.id = 'region-info';
+//   arrayInfo.push(country);
+//   arrayInfo.push(region);
+//   arrayInfo.push(city);
+//   arrayInfo.push(currency_code);
+//   arrayInfo.push(currency_symbol);
+//   arrayInfo.push(sunrise);
+//   arrayInfo.push(sunset);
+//   arrayInfo.push(time_zone);
+//   const arrayNode = makeNodes(arrayInfo);
+//   arrayNode.forEach((element) => unorList.appendChild(element));
+//   return unorList;
+// }
 
 
 function getLocalInfo(objLatLng) {
@@ -228,6 +296,7 @@ function getLocalInfo(objLatLng) {
 function geocode(request) {
   clear();
   clearweatherInfo();
+  container.style.display = 'flex';
   geocoder.geocode(request).then((result) => {
     const { results } = result;
 
