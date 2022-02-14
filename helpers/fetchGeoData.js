6e1aae698190d2e0f4f7892fa242997c9c3313f2
Hugorc10 <hugorc10@hotmail.com>
2022-02-14 16:35:48 -0300

    Adicionado 2 testes

--- helpers/fetchGeoData.js
+++ helpers/fetchGeoData.js
@@ -1,8 +1,18 @@
-async function fetchGeoData(latLng) {
+const fetchGeoData = async (latLng) => {
+  try {
     const { lat, lng } = latLng;
     const key = 'QZ0DTOXXJEJTFUGKC0NJGV2YWU929X9P';
-    const APIStrangeObject = await fetch(`https://api.geodatasource.com/city?key=${key}&format=json&lat=${lat}&lng=${lng}`);
-    const APIObject = await APIStrangeObject.json();
-    console.log(APIObject);
-    return APIObject;
+    const request = await fetch(`https://api.geodatasource.com/city?key=${key}&format=json&lat=${lat}&lng=${lng}`);
+    const data = await request.json();
+    console.log(request);
+    return data;
+  } catch (error) {
+    return new Error('You must provide an argument');
+  }
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = {
+    fetchGeoData,
+  };
 }
